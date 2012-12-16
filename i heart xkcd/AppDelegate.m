b112bb13ab084d675af3ea28ea33685ddb732141
Tom Elliott <mrtom@cantab.net>
2012-12-16 01:46:10 +0000

    Adding the facebook app install ads code

--- i heart xkcd/AppDelegate.m
+++ i heart xkcd/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import <AFNetworking/AFNetworking.h>
+#import <FacebookSDK/FacebookSDK.h>
 
 @implementation AppDelegate
 
@@ -39,6 +40,7 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
     [application setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+    [FBSettings publishInstall:@"331148560332763"];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
